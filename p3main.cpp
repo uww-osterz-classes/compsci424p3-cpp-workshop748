a6d8d7fdf67af82b56c815f6da171e354c406aba
workshop748 <jacobpeacy11@gmail.com>
2024-04-24 14:58:23 -0500

    Update p3main.cpp

--- p3main.cpp
+++ p3main.cpp
@@ -1,6 +1,6 @@
 /* COMPSCI 424 Program 3
    Name:
-   
+
    p3main.cpp: contains the main function for this program.
 
    This is a template. Feel free to edit any of these files, even
@@ -18,27 +18,79 @@
 #include <cstdio> // for sscanf because Dr. Oster likes the C way to
 #include<thread>       // get data from strings better than the C++ way
 #include<mutex>
-
+#include<vector>
 
 using namespace std; // if you want to type out "std::" every time, delete this
 
 /*
    If you want your variables and data structures for the banker's
    algorithm to have global scope, declare them here. This may make
-   the rest of your program easier to write. 
-   
-   Most software engineers say global variables are a Bad Idea (and 
+   the rest of your program easier to write.
+
+   Most software engineers say global variables are a Bad Idea (and
    they're usually correct!), but systems programmers do it all the
    time, so I'm allowing it here.
 */
-int work[NULL];
-bool Finish[NULL];
-int ind =0;
-int max[NULL][NULL];
-int need[NULL][NULL];
-int avali[NULL];
-int alloc[NULL][NULL];
+int ind = 0;
+vector<vector<int>>Request;
+vector<vector<int>> TheMax;
+vector<bool>Finish;
+vector<int>work;
+vector<int>available;
+vector<vector<int>>need;
+vector<vector<int>>Allocation;
 
+
+bool IsSafe(int n)
+{
+    int flag = 1;
+    for (int i = 0; i <n; i++)
+    {
+        if (work[i] == 0)
+        {
+            flag = 0;
+            //this program is not safe to run
+            return false;
+        }
+    }
+    if (flag == 1)
+    {
+        //safe to run 
+        return true;
+    }
+}
+void bankersAlgorithm(int n, int m)
+{
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            //probably not the best way of doing things but im simplifying my code so I can understand it better
+            if (work[i] == 0)
+            {
+                int flag = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (need[i][j] > available[j])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    Finish[ind++] = i;
+                    for (int j = 0; j < m; j++)
+                    {
+                        available[j] += Allocation[i][j];
+                    }
+                    work[i] = 1;
+                }
+            }
+
+        }
+    }
+}
 
 void request()
 {
@@ -52,72 +104,76 @@ void release()
 
 void manualMode(int n, int m)
 {
-string asking="";
-int units,Resources,process;
-char regOrRel [20];
+    string asking = "";
+    int units, Resources, process;
+    char regOrRel[20];
 
-while(getline(cin,asking))
-{
-    if(asking !="end")
+    while (getline(cin, asking))
     {
-    const char* fun = asking.c_str();
-    sscanf(fun, "%s,%d,%*s,%d,%*s,%d",regOrRel,&units,&Resources,&process);
-    if(string(regOrRel)=="request")
-    {
-if(units>0&&units<max[process][Resources])
-{
-if(Resources>0&&Resources<m-1)
-            {
-                if(process>0&&process<n-1)
-                {
-
-                }else{
-                    cout<<"error";
-                    break;
-                }
-            }else{
-            cout<<"error";
-            break;
-            }
-        }else{
-            cout <<"error";
-            break;
-        }
-}
-    }
-     else if(string(regOrRel)=="released")
-     {
-        if(units>0&&units<alloc[process][Resources])
+        if (asking != "end")
         {
-            if(Resources>0&&Resources<m-1)
+            const char* fun = asking.c_str();
+            sscanf_s(fun, "%s,%d,%*s,%d,%*s,%d", regOrRel,8, &units, &Resources, &process);
+            if (string(regOrRel) == "request")
             {
-                if(process>0&&process<n-1)
+                if (units > 0 && units < TheMax[process][Resources])
                 {
+                    if (Resources > 0 && Resources < m - 1)
+                    {
+                        if (process > 0 && process < n - 1)
+                        {
 
-                }else{
-                    cout<<"error";
+                        }
+                        else {
+                            cout << "error";
+                            break;
+                        }
+                    }
+                    else {
+                        cout << "error";
+                        break;
+                    }
+                }
+                else {
+                    cout << "error";
                     break;
                 }
-            }else{
-            cout<<"error";
-            break;
             }
-        }else{
-            cout <<"error";
+        }
+        else if (string(regOrRel) == "released")
+        {
+            if (units > 0 && units < Allocation[process][Resources])
+            {
+                if (Resources > 0 && Resources < m - 1)
+                {
+                    if (process > 0 && process < n - 1)
+                    {
+
+                    }
+                    else
+                    {
+                        cout << "error";
+                        break;
+                    }
+                }
+                else
+                {
+                    cout << "error";
+                    break;
+                }
+            }
+            else
+            {
+                cout << "error";
+                break;
+            }
+        }
+        else
+        {
             break;
         }
-     }
+
     }
-    else{
-        break;
-    }
-}
-
-
-}
-
-void automaticMode(int n,int m)
-{
 
 }
 
@@ -129,7 +185,7 @@ void automaticMode(int n,int m)
     argv[2]: a C-string containing the name of the setup file (and the
              path to the file, if it's not in the current directory)
 */
-int main (int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     // To help you get started, the major steps for the main program
     // are shown here as comments. Feel free to add more comments to
     // help you understand your code, or for any reason. Also feel free
@@ -151,8 +207,8 @@ int main (int argc, char *argv[]) {
     string line;
     int num_resources;
     int num_processes;
-    
-    
+
+
     setup_file.open(argv[2], ios::in);
     if (setup_file.is_open()) {
         // 2. Get the number of resources and processes from the setup
@@ -161,65 +217,65 @@ int main (int argc, char *argv[]) {
         setup_file >> num_resources;
         getline(setup_file, line); // skips the rest of the "resources" line
 
-       
+
         cout << num_resources << " resources" << endl;
-       
+
 
         setup_file >> num_processes;
 
         cout << num_processes << " processes" << endl;
-        
+
         getline(setup_file, line); // skips the rest of the "processes" line
-        getline(setup_file,line);
-//>> will grab a sequence of characters and do the right thing with it.
-        //declaring the 2-D Array outside and declairing them as NUll
-//this is setting up the Available array        
-for(int i =0;i<num_resources;i++)
-{
-    int theNumber;
-    setup_file>>theNumber;
-    avali[i]=theNumber;
-}
-getline(setup_file,line);
-//set up the max array
-for(int i =0;i<num_processes;i++)
-{
-    int TheNumber;
-    for(int j =0;i<num_resources;i++)
-    {
-        setup_file>>TheNumber;
-        max[i][j]=TheNumber;
-    }
-
-}
-getline(setup_file,line);
-//sets up the Allocation array
-for(int i =0;i<num_processes;i++)
-{
-    int TheNumber;
-    for(int j =0;i<num_resources;i++)
-    {
-        setup_file>>TheNumber;
-        alloc[i][j]=TheNumber;
-    }
-
-}
-
-        
-        //initializing every work element to 0
-        for(int i =0;i<num_processes;i++)
+        getline(setup_file, line);
+        //>> will grab a sequence of characters and do the right thing with it.
+                //declaring the 2-D Array outside and declairing them as NUll
+        //this is setting up the Available array        
+        for(int i = 0; i < num_resources; i++)
         {
-            work[i]=0;
-        } 
-//setting up the need fo the program.
-        for(int i=0;i<num_processes;i++)
+            int theNumber;
+            setup_file >> theNumber;
+            available.push_back(theNumber);
+        }
+        getline(setup_file, line);
+        //set up the max array
+        for(int i = 0; i < num_processes; i++)
         {
-            for(int j=0;j<num_resources;j++)
+            int TheNumber;
+            for (int j = 0; i < num_resources; i++)
             {
-                
-                need[i][j]=max[i][j]-alloc[i][j];
+                setup_file >> TheNumber;
+                TheMax.at(i).push_back(TheNumber);
             }
-        }    
+
+        }
+        getline(setup_file, line);
+        //sets up the Allocation array
+        for(int i = 0; i < num_processes; i++)
+        {
+            int TheNumber;
+            for (int j = 0; i < num_resources; i++)
+            {
+                setup_file >> TheNumber;
+                Allocation[i][j] = TheNumber;
+            }
+
+        }
+
+
+        //initializing every work element to 0
+        for (int i = 0; i < num_processes; i++)
+        {
+            work[i] = 0;
+        }
+        //setting up the need fo the program.
+        for (int i = 0; i < num_processes; i++)
+        {
+            for (int j = 0; j < num_resources; j++)
+            {
+
+                need[i][j] = TheMax[i][j] - Allocation[i][j];
+            }
+        }
         // 3. Use the rest of the setup file to initialize the data structures
 
 
@@ -230,54 +286,40 @@ for(int i =0;i<num_processes;i++)
     // 4. Check initial conditions to ensure that the system is
     // beginning in a safe state: see "Check initial conditions"
     // in the Program 3 instructions
-    for(int k=0;k<num_processes;k++)
+    for (int k = 0; k < num_processes; k++)
     {
-        for(int i=0;i<num_resources;i++)
+        for (int i = 0; i < num_resources; i++)
         {
-            //probably not the best way of doing things but im simplifing my code so I can understand it better
-            if(work[i]==0)
+            
+            if (work[i] == 0)
             {
-                int flag =1;
-                for(int j =0; j<num_processes;j++)
+                int flag = 1;
+                for (int j = 0; j < num_processes; j++)
                 {
-                    if(need[i][j]>avali[j])
+                    if (need[i][j] > available[j])
                     {
-                        flag =1;
+                        flag = 1;
                         break;
                     }
                 }
-                if(flag ==0)
-            {
-               Finish[ind++]=i;
-               for(int j=0;j<num_processes;j++)
-               {
-                avali[j]+= alloc[i][j];
-               }
-               work[i]=1;
+                if (flag == 0)
+                {
+                    Finish[ind++] = i;
+                    for (int j = 0; j < num_processes; j++)
+                    {
+                        available[j] += Allocation[i][j];
+                    }
+                    work[i] = 1;
+                }
             }
-            }
-            
-        }
-    }
-    int flag =1;
 
-    for (int i =0;i<num_processes;i++)
-    {
-        if(work[i]==0)
-        {
-            flag=0;
-            cout<<"The given sequence is not safe"<<endl;
-            break;
         }
     }
-    if(flag ==1)
+    int flag = 1;
+    bool safeOrNot = IsSafe(num_processes);
+    if (safeOrNot == false)
     {
-        cout<<"Following is the SAFE sequence"<<endl;
-        for(int i=0;i<num_processes-1;i++)
-        {
-            cout<<" P"<<Finish[i]<<" ->";
-        }
-        cout<<" P"<<Finish[num_processes-1]<<endl;
+        return -1;
     }
 
     // 5. Go into either manual or automatic mode, depending on
@@ -285,21 +327,21 @@ for(int i =0;i<num_processes;i++)
     // as separate methods within this class, as separate classes
     // with their own main methods, or as additional code within
     // this main method.
-    string  mode =argv[1];
-    if(mode == "manual"||mode=="Manual")
+    string  mode = argv[1];
+    if (mode == "manual" || mode == "Manual")
     {
-        manualMode(num_resources,num_processes);
+        manualMode(num_resources, num_processes);
     }
-    else if(mode == "auto"||mode == "auto")
+    else if (mode == "auto" || mode == "Auto")
     {
-        automaticMode(num_resources,num_processes);
+        
     }
     else
     {
-        cout<<" Invalid node. Please try again by spesifing 'manual' or 'automatic'";
+        cout << " Invalid node. Please try again by specifying 'manual' or 'automatic'";
         return 1;
     }
 
-    
+
     return 0; // terminate normally
 }
